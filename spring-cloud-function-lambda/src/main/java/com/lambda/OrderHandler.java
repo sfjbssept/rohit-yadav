d084db04aeab4536c30e3512fe33d67b7bf83aa3
sfjbssept <sfjbssept@gmail.com>
2022-10-27 03:58:18 +0000

    lambda function

---
+++ spring-cloud-function-lambda/src/main/java/com/lambda/OrderHandler.java
@@ -0,0 +1,8 @@
+package com.lambda;
+
+import org.springframework.cloud.function.adapter.aws.SpringBootRequestHandler;
+
+public class OrderHandler extends SpringBootRequestHandler<String, Object> {
+
+}
+ 
